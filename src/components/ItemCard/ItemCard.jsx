ee3aacaca086d7fbfcb7df97af3653ee9aceeec7
HermenStudent <andreshermen1012@gmail.com>
2025-09-04 22:14:19 -0500

    Updated

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,25 +1,26 @@
 import "./ItemCard.css";
+import { useContext } from "react";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 
 function ItemCard({ item, onCardClick, onlikeClick }) {
   const handleCardClick = () => {
     onCardClick(item);
   };
 
+  const { currentUser } = useContext(CurrentUserContext);
+
+  const isLiked =
+    item.likes && currentUser && item.likes.includes(currentUser._id);
+
   const handleLike = () => {
-    if (item.isLiked) {
-      onlikeClick(item._id, "remove");
-    } else {
-      onlikeClick(item._id, "add");
-    }
+    onlikeClick({ id: item._id, isLiked: isLiked });
   };
 
   return (
     <li className="card">
       <h2 className="card__name">{item.name}</h2>
       <button
-        className={`card__like-btn ${
-          item.isLiked ? "card__like-btn_active" : ""
-        }`}
+        className={`card__like-btn ${isLiked ? "card__like-btn_active" : ""}`}
         onClick={handleLike}
       />
       <img
